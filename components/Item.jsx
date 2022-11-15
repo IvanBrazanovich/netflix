e16b2fcc2bea915c874fec845ff4a516d85435cd
Ivan <ivanbrazas@gmail.com>
2022-11-15 11:01:33 -0300

    before netflix card effect

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,32 +1,142 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/components/lists.module.scss";
 import nlogo from "../public/img/n_logo.svg";
 import Image from "next/image";
+import CardPortal from "./CardPortal";
+import useDebounce from "./useDebounce";
 
 const Item = ({ movie }) => {
+  const [portal, setPortal] = useState(false);
+  const [portal2, setPortal2] = useState(false);
+  const [positionArray, setPositionArray] = useState({});
+  const cardRef = useRef();
   const { backdrop_path } = movie;
 
+  const changePortal = () => {
+    const rect = cardRef.current.getBoundingClientRect();
+    const position = {
+      x: rect.x + window.scrollX,
+      y: rect.y + window.scrollY,
+
+      width: rect.width,
+      height: rect.height,
+    };
+    setPositionArray(position);
+    setPortal(true);
+  };
   return (
-    <div className={styles.listItem}>
-      <div type="img_container" className={styles.listItem__imgWrapper}>
-        {Math.random() < 0.7 ? (
-          <div className={styles.nlogo_wrapper}>
-            <Image
-              alt="netflix logo"
-              className={styles.nlogo}
-              src={nlogo}
-              fill
-            />
-          </div>
+    <>
+      <div
+        ref={cardRef}
+        className={styles.listItem}
+        onMouseEnter={() => {
+          setPortal2(true);
+          setTimeout(() => {
+            changePortal();
+          }, 1000);
+        }}
+        onMouseLeave={() => {
+          setPortal2(false);
+          setPortal(false);
+        }}
+      >
+        <div type="img_container" className={styles.listItem__imgWrapper}>
+          {Math.random() < 0.7 ? (
+            <div className={styles.nlogo_wrapper}>
+              <Image
+                alt="netflix logo"
+                className={styles.nlogo}
+                src={nlogo}
+                fill
+              />
+            </div>
+          ) : null}
+          <Image
+            alt="Picture"
+            src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
+            fill
+            objectFit="cover"
+          />
+        </div>
+        {portal && portal2 ? (
+          <CardPortal selector="#cardportal">
+            <div
+              className={styles.cardPortal}
+              style={{
+                top: positionArray.y,
+                left: positionArray.x,
+                minWidth: `${positionArray.width}px`,
+                minHeight: `${positionArray.height}px`,
+                zIndex: 10,
+              }}
+            >
+              <div className={styles.listItemPortal}>
+                <div
+                  type="img_container"
+                  className={styles.listItemPortal__imgWrapper}
+                >
+                  {Math.random() < 0.7 ? (
+                    <div className={styles.nlogo_wrapper}>
+                      <Image
+                        alt="netflix logo"
+                        className={styles.nlogo}
+                        src={nlogo}
+                        fill
+                      />
+                    </div>
+                  ) : null}
+                  <Image
+                    alt="Picture"
+                    src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
+                    fill
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className={styles.cardInfo}>
+                  <div className={styles.features}>
+                    <div className={styles.left}>
+                      <ion-icon name="caret-forward-circle"></ion-icon>
+                      <ion-icon name="add-circle-outline"></ion-icon>{" "}
+                    </div>
+                    <div className={styles.right}>
+                      <ion-icon name="heart-outline"></ion-icon>
+                      <ion-icon name="heart-dislike-outline"></ion-icon>
+                      <ion-icon name="albums-outline"></ion-icon>
+                      <ion-icon name="close-circle-outline"></ion-icon>
+                    </div>
+                  </div>
+
+                  <div className={styles.inlineData}>
+                    <span className={styles.age}>16+</span>
+                    <span className={styles.volumenes}>
+                      {Math.ceil(Math.random() * 10)} Volúmenes
+                    </span>
+                    <span className={styles.quality}>HD</span>
+                  </div>
+
+                  <div className={styles.tags}>
+                    <ul>
+                      <li>De investigación</li>
+                      <li>Crímenes reales</li>
+                      <li>Ovnis</li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div className={styles.moreInfo}>
+                  <div className="wrapper">
+                    <ion-icon name="information-circle-outline"></ion-icon>{" "}
+                    <p>Más información</p>
+                  </div>
+                  <ion-icon name="chevron-forward-outline"></ion-icon>{" "}
+                </div>
+              </div>
+            </div>
+          </CardPortal>
         ) : null}
-        <Image
-          alt="Picture"
-          src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
-          fill
-          objectFit="cover"
-        />{" "}
       </div>
-    </div>
+    </>
   );
 };
 export default Item;
