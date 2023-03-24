8049b343eeecd8e5ba5e6ce896ac02a1ed713fce
Ivan <ivanbrazas@gmail.com>
2023-03-24 13:03:15 -0300

    added head changed to functional

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <title>NETFLIX</title>
-        </Head>
-        <body>
-          <Main />
-          <div id="cardportal" />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <title>NETFLIX</title>
+      <body>
+        <Main />
+        <div id="cardportal" />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
